5a46c41c5565e45214b7c5b9e5267698b2123f38
ujjwaldalal7 <ujjwaldalal1611@gmail.com>
2025-01-25 18:55:55 +0530

    Basic frontend page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
-
+import Navbar from '../components/Navbar';
+import Question from '../components/Question';
 const Home = () => {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        alert('Form submitted');
-    };
-
     return (
+        <>
+        <Navbar/>
         <div className='flex flex-col justify-center items-center flex-1'>
-            <h1 className='text-6xl font-bold text-center'>Welcome to Quiz Portal</h1>
-
-
+            <Question/>
         </div>
+        </>
     );
 };
 
