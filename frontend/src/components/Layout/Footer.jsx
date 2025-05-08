4a96d644f4fb74424385fbd47ca6b22a25ee2d0a
Dhruv Goyal <151380878+DhruvGoyal404@users.noreply.github.com>
2025-05-08 06:56:17 +0530

    Update Footer.jsx

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -8,19 +8,19 @@ const Footer = () => {
           © {new Date().getFullYear()} AirBooking - DBMS Project
         </Typography>
         <Typography variant="body2" align="center" sx={{ mt: 1 }}>
-          <Link href="#" color="inherit" sx={{ mx: 1 }}>
+          <Link href="https://github.com/ujjwaldalal7" color="inherit" sx={{ mx: 1 }}>
             Ujjwal Dalal
           </Link>
           |
-          <Link href="#" color="inherit" sx={{ mx: 1 }}>
+          <Link href="https://github.com/SakshhamTheCoder/" color="inherit" sx={{ mx: 1 }}>
             Sakshham Bhagat
           </Link>
           |
-          <Link href="#" color="inherit" sx={{ mx: 1 }}>
+          <Link href="https://github.com/DhruvGoyal404" color="inherit" sx={{ mx: 1 }}>
             Dhruv Goyal
           </Link>
           |
-          <Link href="#" color="inherit" sx={{ mx: 1 }}>
+          <Link href="https://github.com/JeevantVerma" color="inherit" sx={{ mx: 1 }}>
             Jeevant Verma
           </Link>
         </Typography>
